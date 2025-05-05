f5837e0c7d1c2017d228235f1a4feef44992b3cf
Joni Mäkinen <joni-makinen@live.fi>
2025-05-05 10:49:39 +0300

    Add missing commenting to functions.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
+/* Handles try button clicks
+ *
+ * Is invoked via a click listener
+ *
+ * Params – none
+ * Returns – none
+ */
 function handleClick() {
   // Fetch image data and quote data
-  // $data is a promise, and it looks like this { $imgData, $quoteData }
+  // $data is a promise
   $data = fetchImageAndQuote();
-
-  console.log($data);
 
   // Form image and quote components and add them to the page
   $data
@@ -20,7 +25,6 @@ function handleClick() {
       $quote = $(
         `<blockquote>${$text}<footer>${$author}</footer></blockquote>`
       );
-
       // Set the image element styles
       $img.css({ display: "block", margin: "auto" }); // Prettier extension removes the quotation marks around 'display' and 'margin'?
 
@@ -41,64 +45,14 @@ function handleClick() {
       console.log(error);
     });
 }
-// function handleClick() {
-//   // Fetch image data
-//   // imgData is a promise
-//   $imgData = fetchImage();
 
-//   // Form an image component and add it to the page
-//   // No need for catch block here, errors are handled already in fetchImage
-//   $imgData
-//     .then((data) => {
-//       // Set the image URL and image description in variables
-//       $URL = data.urls.full;
-//       $desc = data.description;
-
-//       // Form an image element with the fetched data
-//       $img = $(`<img src="${$URL}" alt="${$desc}" height="400" />`);
-//       // Set the image element styles
-//       $img.css({ display: "block", margin: "auto" }); // Prettier extension removes the quotation marks around 'display' and 'margin'?
-//       // Make the image element fade in slowly
-//       $img.hide().fadeIn(5000, () => console.log("Image fade-in complete"));
-
-//       // If a previous image element exists, then replace it with the new one...
-//       if ($("img").length) $("img").replaceWith($img);
-//       // ...or if it doesn't exist, then append the new image element to the webpage body
-//       else $("body").append($img);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-
-//   // Fetch quote data
-//   // quoteData is a promise
-//   $quoteData = fetchQuote();
-
-//   // Form a quote component and add it to the page
-//   // No need for catch block here, errors are handled already in fetchQuote
-//   $quoteData
-//     .then((data) => {
-//       // Set the quote text and quote author in variables
-//       $text = data[0].quote;
-//       $author = data[0].author;
-
-//       // Form a blockquote element with the fetched data
-//       $quote = $(
-//         `<blockquote>${$text}<footer>${$author}</footer></blockquote>`
-//       );
-//       // Make the blockquote element fade in slowly
-//       $quote.hide().fadeIn(5000, () => console.log("Quote fade-in complete"));
-
-//       // If a previous blockquote element exists, then replace it with the new one...
-//       if ($("blockquote").length) $("blockquote").replaceWith($quote);
-//       // ...or if it doesn't exist, then append the new blockquote element to the webpage body
-//       else $("body").append($quote);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }
-
+/* Initializes the webpage
+ *
+ * Sets style for the webpage, creates its header and creates the try button
+ *
+ * Params – none
+ * Returns – none
+ */
 function main() {
   // Set the webpage body some style
   $("body").css("text-align", "center");
