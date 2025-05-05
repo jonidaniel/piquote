9a70919887afddaf95ec93a1107b7e3f3bfef694
Joni Mäkinen <joni-makinen@live.fi>
2025-05-05 10:32:19 +0300

    Join fetchImage and fetchQuote functions together as fetchImageAndQuote. This way both, image and quote, appear on the page at the same time.

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,15 +1,29 @@
-/* Fetches a quote using jQuery AJAX
+/* Fetches an image and a quote using jQuery AJAX
  *
- * Utilizes API Ninjas' Quotes API
+ * Utilizes Unsplash API's random photo endpoint and API Ninjas' Quotes API
  *
  * Params – none
- * Returns – quoteData (Promise)
+ * Returns – { $imageData (Promise), $quoteData (Promise) }
  */
-async function fetchQuote() {
-  // Introduce a variable for the quote data
+async function fetchImageAndQuote() {
+  // Introduce variables for image data and quote data
+  // let imgData; // You don't need this line, why? Closure?
   // let quoteData; // You don't need this line, why? Closure?
 
-  // Make an AJAX call
+  // Make an AJAX call to fetch the image data
+  await $.ajax({
+    url: "https://api.unsplash.com/photos/random?client_id=lrVhFg8r0-qGbnC7vCqD_gZE9nG1NAxJG3R42nfk23E",
+    method: "get",
+    success: (data) => {
+      $imgData = data; // You don't need the '$' sign here
+    },
+    error: (error) => {
+      console.log(error.status + " error");
+      $imgData = error; // You don't need the '$' sign here
+    },
+  });
+
+  // Make an AJAX call to fetch the quote data
   await $.ajax({
     url: "https://api.api-ninjas.com/v1/quotes",
     method: "get",
@@ -23,34 +37,62 @@ async function fetchQuote() {
     },
   });
 
-  // Return the quote data (as a promise)
-  return $quoteData;
+  // Return the image data and quote data (as a promise)
+  return { $imgData, $quoteData };
 }
+// /* Fetches a quote using jQuery AJAX
+//  *
+//  * Utilizes API Ninjas' Quotes API
+//  *
+//  * Params – none
+//  * Returns – quoteData (Promise)
+//  */
+// async function fetchQuote() {
+//   // Introduce a variable for the quote data
+//   // let quoteData; // You don't need this line, why? Closure?
 
-/* Fetches an image using jQuery AJAX
- *
- * Utilizes Unsplash API's random photo endpoint
- *
- * Params – none
- * Returns – imgData (Promise)
- */
-async function fetchImage() {
-  // Introduce a variable for the image data
-  // let imgData; // You don't need this line, why? Closure?
+//   // Make an AJAX call
+//   await $.ajax({
+//     url: "https://api.api-ninjas.com/v1/quotes",
+//     method: "get",
+//     headers: { "X-Api-Key": "REBH1bj+cY8AAOSYAQV9+Q==8CkjSeLCASHUYj6l" },
+//     success: (data) => {
+//       $quoteData = data; // You don't need the '$' sign here
+//     },
+//     error: (error) => {
+//       console.log(error.status + " error");
+//       $quoteData = error; // You don't need the '$' sign here
+//     },
+//   });
 
-  // Make an AJAX call
-  await $.ajax({
-    url: "https://api.unsplash.com/photos/random?client_id=lrVhFg8r0-qGbnC7vCqD_gZE9nG1NAxJG3R42nfk23E",
-    method: "get",
-    success: (data) => {
-      $imgData = data; // You don't need the '$' sign here
-    },
-    error: (error) => {
-      console.log(error.status + " error");
-      $imgData = error; // You don't need the '$' sign here
-    },
-  });
+//   // Return the quote data (as a promise)
+//   return $quoteData;
+// }
 
-  // Return the image data (as a promise)
-  return $imgData;
-}
+// /* Fetches an image using jQuery AJAX
+//  *
+//  * Utilizes Unsplash API's random photo endpoint
+//  *
+//  * Params – none
+//  * Returns – imgData (Promise)
+//  */
+// async function fetchImage() {
+//   // Introduce a variable for the image data
+//   // let imgData; // You don't need this line, why? Closure?
+
+//   // Make an AJAX call
+//   await $.ajax({
+//     url: "https://api.unsplash.com/photos/random?client_id=lrVhFg8r0-qGbnC7vCqD_gZE9nG1NAxJG3R42nfk23E",
+//     method: "get",
+//     success: (data) => {
+//       $imgData = data; // You don't need the '$' sign here
+//     },
+//     error: (error) => {
+//       console.log(error.status + " error");
+//       $imgData = error; // You don't need the '$' sign here
+//     },
+//   });
+
+//   // Return the image data (as a promise)
+//   return $imgData;
+// }
