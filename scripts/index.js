f05df4d42540d345a682ca72961ca4f07de3088f
Joni Mäkinen <joni-makinen@live.fi>
2025-05-06 09:41:18 +0300

    Implement soft edges to images.

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,9 @@
  */
 function handleClick(e) {
   // If previous image or quote elements exist, then slide them up
-  if ($("img").length) $("img").slideUp();
-  if ($("blockquote").length) $("blockquote").slideUp();
+  // if ($("img").length) $("img").slideUp();
+  if ($("div").length) $("div").slideUp(1000);
+  if ($("blockquote").length) $("blockquote").slideUp(1000);
 
   // Display 'Loading' text on the try button while the image and quote are loading,
   // also make the button disabled
@@ -28,19 +29,42 @@ function handleClick(e) {
       // Set the quote text and quote author in variables
       $text = data.$quoteData[0].quote;
       $author = data.$quoteData[0].author;
+      //
+      $width = data.$imgData.width;
+      $height = data.$imgData.height;
+
+      // Calculate dimensions for the image container
+      const newHeight = 400;
+      const newWidth = (newHeight / $height) * $width;
 
       // Form image and blockquote elements with the fetched data
-      $img = $(`<img src="${$URL}" alt="${$desc}" height="400" />`);
+      // $img = $(`<img src="${$URL}" alt="${$desc}" height="400" />`);
       $quote = $(
         `<blockquote>${$text}<footer>${$author}</footer></blockquote>`
       );
 
       // Set the image and blockquote elements' styles
-      $img.css({ display: "block", margin: "auto" }); // Prettier extension removes the quotation marks around 'display' and 'margin'?
-      $quote.css({ "margin-up": 20, "margin-left": 50, "margin-right": 50 });
+      // $img.css({ display: "block", margin: "auto" }); // Prettier extension removes the quotation marks around 'display' and 'margin'?
+      // $img.css("box-shadow", 0, 0, 0, 0, "white inset");
+      $quote.css({
+        "margin-up": 20,
+        "margin-left": 50,
+        "margin-right": 50,
+      });
+
+      $div = $("<div></div>");
+      $div.css({
+        margin: "auto",
+        width: newWidth,
+        height: newHeight,
+        "background-image": `url("${$URL}")`,
+        "background-size": "cover",
+        "box-shadow": "0 0 8px 8px white inset",
+      });
 
       // Make the image and blockquote elements fade in slowly
-      $img.hide().fadeIn(4000);
+      // $img.hide().fadeIn(4000);
+      $div.hide().fadeIn(4000);
       $quote.hide().fadeIn(4000);
 
       // Change the try button text back to 'Try',
@@ -49,9 +73,11 @@ function handleClick(e) {
       e.target.disabled = false;
 
       // If a previous image element exists, then replace it with the new one...
-      if ($("img").length) $("img").replaceWith($img);
+      // if ($("img").length) $("img").replaceWith($img);
+      if ($("div").length) $("div").replaceWith($div);
       // ...or if it doesn't exist, then append the new image element to the webpage body
-      else $("body").append($img);
+      // else $("body").append($img);
+      else $("body").append($div);
       // If a previous blockquote element exists, then replace it with the new one...
       if ($("blockquote").length) $("blockquote").replaceWith($quote);
       // ...or if it doesn't exist, then append the new blockquote element to the webpage body
